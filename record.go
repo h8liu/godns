47116d2363a1c90b6dc719b225dee01676291d2e
lonnie <liulonnie@gmail.com>
2012-07-05 19:13:01 -0700

    starting record prob

---
+++ record.go
@@ -0,0 +1,26 @@
+package dns
+
+type RecordProb struct {
+    name *Name
+    types []uint16      
+}
+
+func NewRecordProb(name *Name, types []uint16) *RecordProb {
+    ret := &RecordProb{name:name, types:make([]uint16, len(types))}
+    copy(ret.types, types)
+    return ret
+}
+
+func (p *RecordProb) Title() (name string, meta []string) {
+    return "record", []string{}
+}
+
+func (p *RecordProb) ExpandVia(a Agent) {
+    recur := NewRecurProb(p.name, A)
+    a.SolveSub(recur)
+
+    if recur.Answer == nil {
+        return
+    }
+
+}
